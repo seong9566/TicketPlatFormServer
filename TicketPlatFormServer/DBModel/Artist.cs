33dc549f28dab46da8ecd245e5379366fad365ee
HyeonSeong <dlgustjd9566@gmail.com>
2025-12-16 16:48:17 +0900

    아티스트 커밋 추가

---
+++ TicketPlatFormServer/DBModel/Artist.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TicketPlatFormServer.DBModel;
+
+public partial class Artist
+{
+    public long Id { get; set; }
+
+    public long CategoryId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? ProfileImageUrl { get; set; }
+
+    public string? Description { get; set; }
+
+    public bool? IsActive { get; set; }
+
+    public int SortOrder { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public virtual ICollection<ArtistFollower> ArtistFollowers { get; set; } = new List<ArtistFollower>();
+
+    public virtual TicketCategory Category { get; set; } = null!;
+
+    public virtual ICollection<Event> Events { get; set; } = new List<Event>();
+}
---
+++ TicketPlatFormServer/DBModel/ArtistFollower.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TicketPlatFormServer.DBModel;
+
+public partial class ArtistFollower
+{
+    public long Id { get; set; }
+
+    public long ArtistId { get; set; }
+
+    public long UserId { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public virtual Artist Artist { get; set; } = null!;
+
+    public virtual User User { get; set; } = null!;
+}
